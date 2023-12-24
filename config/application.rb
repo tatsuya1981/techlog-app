3729fe1949af3d0f36e5f5766b4eea400136408c
tatsuya <clarkmaster@icloud.com>
2023-12-24 10:48:10 +0900

    RSpecの初期設定完了！

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module TechlogApp
     config.generators do |g|
       g.assets false          # css,JavaScriptファイルを自動生成しない
       g.helper false          # helperファイルを自動生成しない
+      g.test_framework :rspec,
+        fixtures: false,      #テストDBにレコードを作るfixtureの作成をスキップ
+        view_specs: false,    #ビューファイル用のスペックを作成しない
+        helper_specs: false,  #ヘルパーファイル用のスペックを作成しない
+        routing_specs: false  #ルート用のスペックファイルを作成しない
     end
   end
 end
